78361b3008137dd8c1bac35415de2711d473c6cf
dktlh <724279138@qq.com>
2016-01-12 11:12:14 +0800

    base

--- app/src/main/java/com/ld/tao/lhweather/base/BaseFragment.java
+++ app/src/main/java/com/ld/tao/lhweather/base/BaseFragment.java
@@ -1,10 +1,10 @@
 package com.ld.tao.lhweather.base;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
